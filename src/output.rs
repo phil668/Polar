fa4003507bc5b99d3da32ed7044150adb0619af6
liuyue <1308972436@qq.com>
2024-01-20 01:25:16 +0800

    feat: add tildes for each line

--- src/output.rs
+++ src/output.rs
@@ -1,19 +1,40 @@
-use std::io::stdout;
+use std::io::{stdout, Write};
 
-use crossterm::{execute, terminal};
+use crossterm::{cursor, execute, terminal};
 
-pub struct Output;
+pub struct Output {
+    // (columns,rows)
+    win_size: (usize, usize),
+}
 
 impl Output {
     pub fn new() -> Self {
-        Output
+        let win_size = terminal::size()
+            .map(|(x, y)| (x as usize, y as usize))
+            .unwrap();
+        Output { win_size }
+    }
+
+    fn draw_rows(&self) {
+        let rows = self.win_size.1;
+        for i in 0..rows {
+            print!("~");
+            if i < rows - 1 {
+                println!("\r")
+            }
+            // 保证所有缓冲区的内容都被输出到终端上
+            stdout().flush().unwrap();
+        }
     }
 
     pub fn clear_screen() -> crossterm::Result<()> {
-        execute!(stdout(), terminal::Clear(terminal::ClearType::All))
+        execute!(stdout(), terminal::Clear(terminal::ClearType::All))?;
+        execute!(stdout(), cursor::MoveTo(0, 0))
     }
 
     pub fn refresh_screen(&self) -> crossterm::Result<()> {
-        Self::clear_screen()
+        Self::clear_screen()?;
+        self.draw_rows();
+        execute!(stdout(), cursor::MoveTo(0, 0))
     }
 }
